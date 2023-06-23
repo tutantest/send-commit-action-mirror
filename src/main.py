20df70940cae3f5b4b02c5fc2228d8df63332a9e
afajardom <afajardo@enagas.es>
2023-06-23 12:46:18 +0200

    test for getting files content from pull request

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import os
+import Github
 
 def main():
   #Get inputs values
@@ -10,6 +11,22 @@ def main():
   target_repo = os.environ["INPUT_TARGET-REPO"]
   result = ""
   result = author + " - " + source_repo + " - " + target_repo
+
+  github = Github(token="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")
+  repo = github.get_repo("tutantest/back-mirror-test", lazy=False)
+
+  pr = repo.get_pull(1)
+
+  commits = pr.get_commits()
+
+  for commit in commits:
+    files = commit.files
+    for file in files:
+        filename = file.filename
+        print("nombre fichero: " + filename)
+        contents = repo.get_contents(filename, ref=commit.sha).decoded_content
+        print("contenido: " + contents)
+
   #Set the output value
   print(f"::set-output name=result::{result}")
 
