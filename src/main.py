0a5aff5222855554f7ba03dc8f3eea880b0d25a0
tutantest <109337854+tutantest@users.noreply.github.com>
2023-06-23 13:05:05 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -21,7 +21,7 @@ def main():
     print("current pull: " + pull.number)
     pulls_numbers_list.append(pull.number)
 
-  print("Pulls: " + len(pulls_numbers_list))
+  print("Pulls: " + str(len(pulls_numbers_list)))
     
   pr = repo.get_pull(pulls_numbers_list[0])
 
