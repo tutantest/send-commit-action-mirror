8a54b85f57a0828ba6f74a52ce1a89c65300c494
tutantest <109337854+tutantest@users.noreply.github.com>
2023-06-23 11:21:36 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,15 +1,17 @@
 import os
 
-#Get inputs values
-#commits-author
-#source-repo-pull-request
-#target-repo
-author = os.environ["INPUT_COMMITS-AUTHOR"]
-source_repo = os.environ["INPUT_SOURCE-REPO-PULL-REQUEST"]
-target_repo = os.environ["INPUT_TARGET-REPO"]
+def main():
+  #Get inputs values
+  #commits-author
+  #source-repo-pull-request
+  #target-repo
+  author = os.environ["INPUT_COMMITS-AUTHOR"]
+  source_repo = os.environ["INPUT_SOURCE-REPO-PULL-REQUEST"]
+  target_repo = os.environ["INPUT_TARGET-REPO"]
+  result = ""
+  result = author + " - " + source_repo + " - " + target_repo
+  #Set the output value
+  print(f"::set-output name=result::{result}")
 
-result = ""
-result = author + " - " + source_repo + " - " + target_repo
-
-#Set the output value
-print(f"::set-output name=result::{result}")
+if __name__ == "__main__":
+  main()
