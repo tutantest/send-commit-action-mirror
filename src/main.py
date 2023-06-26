8e85b681efaf2096b3eaa335b92ee9e833c3da1c
tutantest <109337854+tutantest@users.noreply.github.com>
2023-06-26 14:59:07 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -14,8 +14,8 @@ def main():
   source_repo = os.environ["INPUT_SOURCE-REPO-PULL-REQUEST"]
   target_repo = os.environ["INPUT_TARGET-REPO"]
   usuario = os.environ["INPUT_USUARIO"]
-  #pull_number = os.environ["INPUT_PULL-NUMBER"]
-  pull_number = 6
+  pull_number = os.environ["INPUT_PULL-NUMBER"]
+  print(pull_number)
   print("USUARIO: " + usuario)
   result = ""
   result = author + " - " + source_repo + " - " + target_repo
@@ -52,7 +52,7 @@ def main():
   #  pulls_numbers_list.append(pull.number)
 
   #print("Pulls: " + str(len(pulls_numbers_list)))
-  print("Recuperando Pull Request " + str(pull_number))
+  #print("Recuperando Pull Request " + str(pull_number))
   #pr = repo.get_pull(pull_number)
   pr = source_repository.get_pull(pull_number)
   
